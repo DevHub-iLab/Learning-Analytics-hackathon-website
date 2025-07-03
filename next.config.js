0c5fa8a91d88f610bf5be77b18f9c60f9f12e540
PuttTim <yannaputt@gmail.com>
2025-07-04 02:53:56 +0800

    removed github deployment related configs

--- next.config.js
+++ next.config.js
@@ -1,22 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  trailingSlash: true,
-  distDir: "out",
-  basePath: "/NTUxATLAS-Hackathon", 
-  assetPrefix: "/NTUxATLAS-Hackathon/",
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-  experimental: {
-    optimizeCss: false,
-  },
+	output: "export",
+	trailingSlash: true,
+	distDir: "out",
+	basePath: "",
+	assetPrefix: "",
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	images: {
+		unoptimized: true,
+	},
+	experimental: {
+		optimizeCss: false,
+	},
 }
 
 module.exports = nextConfig
