f002ea024802c999d36451da0510117d0d456757
PuttTim <yannaputt@gmail.com>
2025-07-04 03:00:43 +0800

    ⚡ updated meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,19 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next"
+import "./globals.css"
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+	title: "NTU x Imperial College London: Learning Analytics Hackathon",
+	description: "NTU NALA x Imperial College London Hackathon. Create the future of learning analytics with us!",
 }
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode
+	children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<body>{children}</body>
+		</html>
+	)
 }
