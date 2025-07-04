8e71f56727009640adc77106526080fb0da6301d
xerri <tanweixin2004@gmail.com>
2025-07-04 10:39:45 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,9 @@ export default function RootLayout({
         <div className="flex-1 flex flex-col">{children}</div>
         <footer className="w-full bg-[#0F172A] py-6 mt-20 border-t border-slate-800">
           <div className="text-center text-white text-base font-medium tracking-wide">
-            Made with <span className="inline-block align-middle text-red-500 mx-1">❤️</span>
+            A collaboration between NTU and Imperial College London.
+			<br />
+			Made with <span className="inline-block align-middle text-red-500 mx-1">❤️</span>
             by <span className="inline-block align-middle font-bold" style={{ color: "#ff9500" }}>DEVHUB_</span>
           </div>
         </footer>
